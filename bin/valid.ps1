87eac54cd94f3670dce06e7dd60e2c0b9ad2030b
dodo <perfectdodo@gmail.com>
2019-09-01 12:37:41 +0800

    valid.bat instead of powershell

--- bin/valid.ps1
+++
@@ -1,6 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$validator = "$env:SCOOP_HOME/supporting/validator/bin/validator.exe"
-$schema="$env:SCOOP_HOME/schema.json"
-$bucket = "$psscriptroot/.." # checks the parent dir
-
-Invoke-Expression -Command "$validator $schema $($args | ForEach-Object { "$_" })"
