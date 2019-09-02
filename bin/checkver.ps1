a6787401946581a26d22c6dc19ded48e2f8c407c
dodo <perfectdodo@gmail.com>
2019-09-03 06:16:56 +0800

    todotxt: Remove it because extras has it

--- bin/checkver.ps1
+++
@@ -1,4 +0,0 @@
-if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
-$bucket = "$psscriptroot/.." # checks the parent dir
-Invoke-Expression -command "$checkver -dir '$bucket' $($args | ForEach-Object { "$_ " })"
