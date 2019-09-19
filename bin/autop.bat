d5ab19f0ed8ed2753a3f2ae772583f3d4108b3cc
dodo <perfectdodo@gmail.com>
2019-09-19 15:37:43 +0800

    assist utilities: use pwsh if there is.

--- bin/autop.bat
+++ bin/autop.bat
@@ -3,7 +3,9 @@ setlocal
 
 set UPSTREAM=dodorz/scoop-bucket:master
 for /f %%i in ('scoop which scoop') do set AUTOPR=%%~dpiauto-pr.ps1
-for /f %%i in ('scoop which powershell') do set PWSH=%%i
+for /f %%i in ('scoop which pwsh') do set PWSH=%%i
+if "%PWSH%x" == "x" for /f %%i in ('scoop which powershell') do set PWSH=%%i
+
 for %%i in (%0) do set DIR=%%~dpi..
 
 %PWSH% -noprofile -ex unrestricted -f %AUTOPR% -dir %DIR% -upstream %UPSTREAM%  -p 
--- bin/update.bat
+++ bin/update.bat
@@ -5,7 +5,9 @@ for /f %%i in ('scoop which scoop') do (
     set CHECKVER=%%~dpicheckver.ps1
     set FORMAT=%%~dpiformatjson.ps1
 )
-for /f %%i in ('scoop which powershell') do set PWSH=%%i
+for /f %%i in ('scoop which pwsh') do set PWSH=%%i
+if "%PWSH%x" == "x" for /f %%i in ('scoop which powershell') do set PWSH=%%i
+
 for %%i in (%0) do set DIR=%%~dpi..
 
 if "x%~1" == "x" goto updall
