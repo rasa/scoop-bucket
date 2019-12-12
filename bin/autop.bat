4812856a509cf8e45ff98493cfc7e4e132a4ac83
dodo <perfectdodo@gmail.com>
2019-12-12 10:21:31 +0800

    autop: just push, don't update

--- bin/autop.bat
+++ bin/autop.bat
@@ -1,11 +1,15 @@
 @echo off
 setlocal
 
-set UPSTREAM=dodorz/scoop-bucket:master
-for /f %%i in ('scoop which scoop') do set AUTOPR=%%~dpiauto-pr.ps1
-for /f %%i in ('scoop which pwsh') do set PWSH=%%i
-if "%PWSH%x" == "x" for /f %%i in ('scoop which powershell') do set PWSH=%%i
-
 for %%i in (%0) do set DIR=%%~dpi..
-
-%PWSH% -noprofile -ex unrestricted -f %AUTOPR% -dir %DIR% -upstream %UPSTREAM%  -p 
+cd %DIR%
+git pull origin master
+git checkout master
+for /f %%i in ('git diff --name-only') do (
+    git add %%i
+    for /f %%v in ('jq .version %%i') do (
+        git commit -m "%%~ni: Update to version %%v"
+    )
+)
+git push origin master
+git checkout -f master
